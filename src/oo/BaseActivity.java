670a77de29306fdea8c3db287a99111e3e79a494
lcn1988 <lcn1988@126.com>
2014-08-08 11:03:34 +0800

    6666

--- src/oo/BaseActivity.java
+++ src/oo/BaseActivity.java
@@ -1,7 +1,8 @@
 package oo;
 
 import android.app.Activity;
-
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class BaseActivity extends Activity {
@@ -10,6 +11,12 @@ public class BaseActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	public PendingIntent createPendingResult(int requestCode, Intent data, int flags) {
+		// TODO Auto-generated method stub
+		return super.createPendingResult(requestCode, data, flags);
 	}
 
 }
